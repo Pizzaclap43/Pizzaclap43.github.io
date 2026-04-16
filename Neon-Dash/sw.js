f14cfbcff0d86ba5161d93fb644fb424d2dcfe32
Pizzaclap43 <adibabouakar@gmail.com>
2026-04-16 14:33:49 -0400

    Update sw.js

--- Neon-Dash/sw.js
+++ Neon-Dash/sw.js
@@ -1,2 +1,28 @@
-// Service Worker para permitir la instalación de la PWA
-self.addEventListener('fetch', function(event) {});
+const CACHE_NAME = 'neon-dash-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './favicon.png'
+];
+
+// Instalar el Service Worker y guardar los archivos en caché
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Archivos guardados en caché');
+        return cache.addAll(assets);
+      })
+  );
+});
+
+// Responder con los archivos del caché cuando no hay conexión
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
