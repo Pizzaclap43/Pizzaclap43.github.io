fbad0a73cb8d786a014af2ca1145af2b2ae47df9
Pizzaclap43 <adibabouakar@gmail.com>
2026-04-16 18:37:55 -0400

    Update sw.js

--- Neon-Dash/sw.js
+++ Neon-Dash/sw.js
@@ -3,28 +3,40 @@ const assets = [
   './',
   './index.html',
   './manifest.json',
-  './favicon.png'
-  './coin.wav'
+  './favicon.png',
+  './coin.wav',
   './hit.wav'
 ];
 
-// Instalar el Service Worker y guardar los archivos en caché
+// Instalar el Service Worker y guardar TODO en caché (incluyendo audios)
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Archivos guardados en caché');
+        console.log('Archivos y audios guardados en caché');
         return cache.addAll(assets);
       })
   );
 });
 
-// Responder con los archivos del caché cuando no hay conexión
+// Responder desde el caché
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
         return response || fetch(event.request);
       })
+  );
+});
+
+// Limpiar cachés antiguos (opcional pero recomendado)
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
   );
 });
