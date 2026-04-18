766db3c9d41c131cab4d0f30c382b4e28bba6035
Pizzaclap43 <adibabouakar@gmail.com>
2026-04-17 22:19:46 -0400

    Update sw.js

--- Neon-Dash/sw.js
+++ Neon-Dash/sw.js
@@ -1,25 +1,23 @@
-const CACHE_NAME = 'neon-dash-v10';
+const CACHE_NAME = 'neon-dash-v7';
 const assets = [
   './',
   './index.html',
   './manifest.json',
   './favicon.png',
   './coin.wav', 
-  './hit.wav'
+  './hit.wav',
+  './death.wav'
 ];
 
-// Instalación forzada
 self.addEventListener('install', event => {
   self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      // Usamos addAll que es más estricto para asegurar que TODO se guarde
       return cache.addAll(assets);
     })
   );
 });
 
-// Activación y limpieza de versiones fallidas
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => {
@@ -30,13 +28,11 @@ self.addEventListener('activate', event => {
   );
 });
 
-// Estrategia: Primero Caché, si no hay, busca en Red
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
     }).catch(() => {
-      // Esto evita que el juego se ralentice si falla la red
       return new Response('', { status: 404, statusText: 'Offline' });
     })
   );
