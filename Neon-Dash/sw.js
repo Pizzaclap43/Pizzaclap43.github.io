4c8837b6f75c4e294152c1ee9212610fef1562e9
Pizzaclap43 <adibabouakar@gmail.com>
2026-04-25 17:06:47 -0400

    Update service worker cache version to v29

--- Neon-Dash/sw.js
+++ Neon-Dash/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'neon-dash-v28'; // Subimos a v28
+const CACHE_NAME = 'neon-dash-v29'; // Subimos a v29
 const assets = [
   './',
   './index.html',
@@ -19,7 +19,7 @@ self.addEventListener('install', event => {
   );
 });
 
-// Activación: Borra CUALQUIER caché que no sea la v28
+// Activación: Borra CUALQUIER caché que no sea la v29
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => {
