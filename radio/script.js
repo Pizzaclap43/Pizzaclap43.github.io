b1287b40cfa73c8edae716747d8978b323e9f043
Pizzaclap43 <adibabouakar@gmail.com>
2026-07-07 09:50:57 -0400

    Add new radio station and update list

--- radio/script.js
+++ radio/script.js
@@ -43,7 +43,8 @@ const stationsData = [
     { id: 41, name: "Exitos (Puerto Ordaz) 90.5 FM", url: "https://acp4.lorini.net:10109/stream" },
     { id: 42, name: "Onda (Puerto Ordaz) 88.1 FM", url: "https://acp4.lorini.net:10111/stream" },
     { id: 43, name: "Union Radio 1090 AM", url: "https://acp4.lorini.net:2090/stream" },
-    { id: 44, name: "YVKE Mundial Radio 550 AM/94.5 FM", url: "https://radiomundial.com.ve/streaming/" }
+    { id: 44, name: "YVKE Mundial Radio 550 AM/94.5 FM", url: "https://radiomundial.com.ve/streaming/" },
+    { id: 45, name: "RNV (Radio Nacional de Venezuela) 91.1 FM", url: "https://guri.tepuyserver.net/8048/stream" }
 ];
 
 let favorites = JSON.parse(localStorage.getItem('radioPizzaFavs')) || [];
