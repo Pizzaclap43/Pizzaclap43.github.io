5e38bb1ed0689e0b0ed5a89c1160c7d1257839dc
Pizzaclap43 <adibabouakar@gmail.com>
2026-07-15 19:04:05 -0400

    Fix URL for ShowVen Radio 92.9 FM

--- radio/script.js
+++ radio/script.js
@@ -52,7 +52,7 @@ const stationsData = [
     { id: 50, name: "Victoria 103.9 FM", url: "https://sapircast.caster.fm:13998/JCNN8?token=82ed7b04dc883cf719ec4ef217d2c927" },
     { id: 51, name: "Caliente 103.3 FM", url: "https://stream-179.surfernetwork.com/yc81fn5wgm8uv?zt=eyJhbGciOiJIUzI1NiJ9.eyJzdHJlYW0iOiJ5YzgxZm41d2dtOHV2IiwiaG9zdCI6InN0cmVhbS0xNzkuc3VyZmVybmV0d29yay5jb20iLCJydHRsIjo1LCJqdGkiOiJhUnJ6MUlwMlFXV2FwNnZ4Ni1MZnJRIiwiaWF0IjoxNzg0MDgzNTA1LCJleHAiOjE3ODQwODM1NjV9.qIor6GBMGJOtp0WptFDI85zdTxn-B6Xo_n1ogoYCAnI" },
     { id: 52, name: "Costa de Oro 90.7 FM", url: "https://stream-177.surfernetwork.com/a07x8r77cm8uv?zt=eyJhbGciOiJIUzI1NiJ9.eyJzdHJlYW0iOiJhMDd4OHI3N2NtOHV2IiwiaG9zdCI6InN0cmVhbS0xNzcuc3VyZmVybmV0d29yay5jb20iLCJydHRsIjo1LCJqdGkiOiJnQkVmbmlWaFJ3S2JRWmFielR3X0ZnIiwiaWF0IjoxNzg0MDgzNjMyLCJleHAiOjE3ODQwODM2OTJ9.8WS5cj29IAg_Qwtv5w_b28FiHru83LR7Vz5oS7zj6hY" },
-    { id: 53, name: "ShowVen Radio 92.9 FM", url: "https://cp2.lorini.net:29004/stream" }
+    { id: 53, name: "ShowVen Radio 92.9 FM", url: "https://acp2.lorini.net:29004/stream" }
 ];
 
 let favorites = JSON.parse(localStorage.getItem('radioPizzaFavs')) || [];
