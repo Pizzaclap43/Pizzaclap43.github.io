ec134246c4a9e941437207b30821329b249acb45
Pizzaclap43 <adibabouakar@gmail.com>
2026-07-16 14:51:12 -0400

    Update cache name to force final update

--- radio/sw.js
+++ radio/sw.js
@@ -1,5 +1,5 @@
 // Cambiado a v3 para forzar la actualización definitiva
-const CACHE_NAME = 'radio-pizza-v9'; 
+const CACHE_NAME = 'radio-pizza-v10'; 
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
