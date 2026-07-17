0546df2439f86c06ea35158c31e0e27ed8302892
Pizzaclap43 <adibabouakar@gmail.com>
2026-07-17 17:25:34 -0400

    Update cache version to v17 in service worker

--- radio/sw.js
+++ radio/sw.js
@@ -1,5 +1,5 @@
 // Cambiado a v3 para forzar la actualización definitiva
-const CACHE_NAME = 'radio-pizza-v16'; 
+const CACHE_NAME = 'radio-pizza-v17'; 
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
