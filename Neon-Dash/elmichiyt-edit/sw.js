3ecefc037d41d5df5b5dbed76589603d6c155fc8
ElmichiYT <juanbon3032@gmail.com>
2026-07-18 19:16:54 -0600

    Neon Dash Updated by ElmichiYT

---
+++ Neon-Dash/elmichiyt-edit/sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'neon-dash-v39';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './favicon.png',
+  './coin.wav', 
+  './hit.wav',
+  './death.wav',
+  './verisimilitude.mp3'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      const fetchPromise = fetch(event.request).then(networkResponse => {
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, networkResponse.clone());
+        });
+        return networkResponse;
+      });
+      return cachedResponse || fetchPromise;
+    })
+  );
+});
